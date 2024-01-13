a2736a55a7fcae4da4415fd267debee0f330f7a4
Amit gupta <billamitgupta@gmail.com>
2024-01-13 19:36:02 +0530

    amit project description

--- 01.javascript/code/00.ToRunLive/index.js
+++ 01.javascript/code/00.ToRunLive/index.js
@@ -1,1 +1,58 @@
-console.log("Run JavaScript Code Here.");
+// console.log("Run JavaScript Code Here.");
+// const num = [5,6,8];
+// num.push(10);
+// console.log(num);
+// let prog = "javaScript";
+
+// myName("Amit");
+
+// function myName(x) {
+//     console.log(`my name is ${x}`);
+// }
+
+// (function private() {
+//     console.log(`hi there i am using ${prog}`)
+    
+//     window.private = function()
+//     {
+//         private();
+//     } 
+   
+// })();
+// window.private();
+
+// (function(){
+//     var name = IIFE;
+//     console.log(name); 
+// })();
+
+// window.p
+
+// function nams(x=0 , Name){
+//     console.log(x);
+//     console.log(Name);
+
+// }
+// nams(undefined,"amt");
+
+// var num = 1;
+// function outer()
+// {
+//     //var num = 10;
+//     function inner()
+//     {
+//         //var num =100;
+//         console.log(num);
+//     }
+//     return inner;   
+// }
+
+
+// outer()();
+
+var isRaining = false;
+var isSnowing = true;
+
+var ans = isRaining ? "take an umberalla " : isSnowing ? "wear a coat" : "enjoy the weather";
+
+console.log(ans);
