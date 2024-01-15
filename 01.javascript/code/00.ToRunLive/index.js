9994b20b56ca10825c6d4e93e5dacd6f384614d7
Rituraj Goswami <goswami.rituraj03@gmail.com>
2024-01-15 10:37:55 +0530

    Rituraj_ClubConnect changes made

--- 01.javascript/code/00.ToRunLive/index.js
+++ 01.javascript/code/00.ToRunLive/index.js
@@ -1,3 +1,4 @@
+console.log("Run JavaScript Code Here.");
 var name;
 let number;
 const address = [1,2,3];
@@ -6,5 +7,5 @@ console.log(address);
 
 var name = "Human";
 function hoisting(){
-    
+
 }
