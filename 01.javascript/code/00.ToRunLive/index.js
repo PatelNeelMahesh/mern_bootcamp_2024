ac8a22a58f270f641ec700cbc640ceee2daa0343
haiderali1001 <haiderali.1001haider@gmail.com>
2024-01-15 09:38:30 +0530

    Added project initial report and objectives

--- 01.javascript/code/00.ToRunLive/index.js
+++ 01.javascript/code/00.ToRunLive/index.js
@@ -1,1 +1,43 @@
 console.log("Run JavaScript Code Here.");
+var username="Haider";
+      var no = `Username is ${username} bye`;
+      console.log(no);
+
+function m(x=2,y=3){
+    return x*y;
+}
+const p = m(4,5);
+console.log(p);
+
+
+(function () {
+    var privateVar = "I am private";
+    function privateFunction() {
+      console.log(privateVar);
+    }
+  
+    window.publicFunction = function () {
+      privateFunction();
+    };
+  })();
+
+  (function() {
+    var mes="yo .......";
+    console.log(mes);
+  })();
+  
+  // Using the publicly exposed function
+  window.publicFunction(); // Logs: 'I am private'
+
+
+  function outer(){
+    var nu=10;
+    function inner()
+    {
+        var nu = 100;
+        console.log(nu);
+    }
+    inner();
+  }
+  outer();
+//   console.log(nu);
