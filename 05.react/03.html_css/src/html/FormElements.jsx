50d5ab0252fc584381506a01897cb63f47f63bb9
unknown <pneel361@gmail.com>
2024-01-29 07:13:43 +0530

    added form details for html css

---
+++ 05.react/03.html_css/src/html/FormElements.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './FormElements.css';
+
+function FormElements() {
+  return (
+    <form className="form-elements">
+      {/* Text input */}
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+
+      {/* Textarea */}
+      <label>
+        Message:
+        <textarea name="message" />
+      </label>
+
+      {/* Select dropdown */}
+      <label>
+        Country:
+        <select name="country">
+          <option value="usa">USA</option>
+          <option value="canada">Canada</option>
+          {/* Additional options */}
+        </select>
+      </label>
+
+      {/* Radio buttons */}
+      <label>
+        <input type="radio" name="gender" value="male" /> Male
+      </label>
+      <label>
+        <input type="radio" name="gender" value="female" /> Female
+      </label>
+
+      {/* Checkbox */}
+      <label>
+        <input type="checkbox" name="subscribe" /> Subscribe to newsletter
+      </label>
+
+      {/* Submit button */}
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default FormElements;
