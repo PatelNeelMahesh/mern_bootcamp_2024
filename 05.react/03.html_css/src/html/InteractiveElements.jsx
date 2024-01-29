a6e79762f8522a146112b5115b9dcdc7f0a2bb21
unknown <pneel361@gmail.com>
2024-01-29 07:20:57 +0530

    added incrective elements details for html css

---
+++ 05.react/03.html_css/src/html/InteractiveElements.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './InteractiveElements.css';
+
+function InteractiveElements() {
+  return (
+    <div className="interactive-elements">
+      {/* Button */}
+      <button className="btn btn-primary">Primary Button</button>
+
+      {/* Link */}
+      <a href="https://codinggita.com" className="link">Visit Codinggita.com</a>
+    </div>
+  );
+}
+
+export default InteractiveElements;
